68a66bdd2f2bc5d27b608e28d2b9c96621b7f13f
Cristian Bittencourt Candia <cbittenc@everis.com>
2021-03-16 10:04:59 -0300

    Login and User Register views updated

--- app_indicar/lib/components/login.dart
+++ app_indicar/lib/components/login.dart
@@ -14,83 +14,96 @@ class Login extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(bottom: 30),
-                    child: Image.asset(
-                      "images/indicar Logotipo.png",
-                      width: 80,
-                      height: 50,
-                    )),
-                Material(
-                  elevation: 10,
-                  color: Colors.transparent,
-                    child: Container(
-                      child: TextField(
-                        autofocus: false,
-                        keyboardType: TextInputType.emailAddress,
-                        style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "E-mail",
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
+            child: Material(
+              elevation: 10,
+              child: Container(
+                width: 400,
+                color: Colors.white,
+                padding: EdgeInsets.fromLTRB(15, 25, 15, 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(bottom: 30),
+                        child: Image.asset(
+                          "images/indicar Logotipo.png",
+                          width: 80,
+                          height: 50,
+                        )),
+                    Container(
+                      width: 380,
+                      padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                      child: Material(
+                        color: Colors.white,
+                          child: Container(
+                            child: TextField(
+                              autofocus: false,
+                              keyboardType: TextInputType.emailAddress,
+                              style: TextStyle(fontSize: 20),
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                              hintText: "E-mail",
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        )),
+                      ),
                     ),
-                  )),
-                ),
-                Material(
-                  elevation: 10,
-                  color: Colors.transparent,
-                  child: Container(
+                    Material(
                       color: Colors.transparent,
-                      margin: const EdgeInsets.only(top: 10),
-                      child: TextField(
-                        obscureText: true,
-                        keyboardType: TextInputType.visiblePassword,
-                        style: TextStyle(fontSize: 20,),
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                            hintText: "Senha",
-                            filled: true,
-                            fillColor: Colors.white,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(20.0)))),
-                      )),
+                      child: Container(
+                          color: Colors.white,
+                          margin: const EdgeInsets.only(top: 0),
+                          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                          child: TextField(
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                            style: TextStyle(fontSize: 20,),
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                                hintText: "Senha",
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+                          )),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(top: 10),
+                        padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
+                        child: ElevatedButton(
+                          child: Text("Entrar", style: TextStyle(fontSize: 18)),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.orangeAccent,
+                              elevation: 10,
+                              onPrimary: Colors.white,
+                              padding: EdgeInsets.all(18),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                          onPressed: () {
+                            print("clicou");
+                            Navigator.pushNamed(context, '/home');
+                          },
+                        )),
+                    Container(
+                        margin: const EdgeInsets.only(top: 15),
+                        child: TextButton(
+                          child: Text("Criar conta", 
+                              style: TextStyle(color: Colors.blue, 
+                              decoration: TextDecoration.underline,
+                              fontSize: 16,
+                              ),
+                            ),
+                          style: TextButton.styleFrom(primary: Colors.orangeAccent),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/user_register');
+                          },
+                        )),
+                  ],
                 ),
-                Container(
-                    margin: const EdgeInsets.only(top: 10),
-                    child: ElevatedButton(
-                      child: Text("Entrar", style: TextStyle(fontSize: 18)),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.orangeAccent,
-                          elevation: 10,
-                          onPrimary: Colors.white,
-                          padding: EdgeInsets.all(18),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                      onPressed: () {
-                        print("clicou");
-                        Navigator.pushNamed(context, '/home');
-                      },
-                    )),
-                Container(
-                    margin: const EdgeInsets.only(top: 15),
-                    child: TextButton(
-                      child: Text("Criar conta"),
-                      style: TextButton.styleFrom(primary: Colors.orangeAccent),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/user_register');
-                      },
-                    )),
-              ],
+              ),
             ),
           ),
         ),
--- app_indicar/lib/components/user_register.dart
+++ app_indicar/lib/components/user_register.dart
@@ -20,158 +20,177 @@ class UserRegister extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.fromLTRB(25, 40, 25, 10),
+        body: Material(
+          color: Colors.white,
           child: Container(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Material(
-                    elevation: 7,
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(const Radius.circular(10.0)),
-                    child: Container(
-                        child: TextField(
-                      autofocus: true,
-                      keyboardType: TextInputType.name,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                                Icons.person,
-                                color: Colors.black,
-                              ),
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "Primeiro nome",
-                        filled: true,
-                        fillColor: Colors.white,
-                        focusedBorder: InputBorder.none,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    )),
-                  ),
-                  Padding(padding: EdgeInsets.all(3)),
-                  Material(
-                    elevation: 7,
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.all(const Radius.circular(10.0)),
-                    child: Container(
-                        margin: const EdgeInsets.only(top: 0),
-                        child: TextField(
-                          obscureText: true,
-                          keyboardType: TextInputType.phone,
+            margin: new EdgeInsets.fromLTRB(10, 30, 10, 30),
+            width: 430,
+            child: Material(
+              elevation: 20,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              child: Container(
+                width: 380,
+                padding: EdgeInsets.fromLTRB(15, 25, 15, 25),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Material(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.all(const Radius.circular(10.0)),
+                        child: Container(
+                            child: TextField(
+                          autofocus: true,
+                          keyboardType: TextInputType.name,
                           style: TextStyle(fontSize: 20),
                           decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.phone_android,
-                                color: Colors.black,
-                              ),
-                              contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                              hintText: "Telefone",
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: InputBorder.none,
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                        )),
-                  ),
-                  Padding(padding: EdgeInsets.all(3)),
-                  Material(
-                    elevation: 7,
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.all(const Radius.circular(10.0)),
-                    child: Container(
-                        margin: const EdgeInsets.only(top: 0),
-                        child: TextField(
-                          obscureText: true,
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(fontSize: 20),
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.email,
-                                color: Colors.black,
-                              ),
-                              contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                              hintText: "E-mail",
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: InputBorder.none,
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                        )),
-                  ),
-                  Padding(padding: EdgeInsets.all(3)),
-                  Material(
-                    elevation: 7,
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.all(const Radius.circular(10.0)),
-                    child: Container(
-                        margin: const EdgeInsets.only(top: 0),
-                        child: TextField(
-                          obscureText: true,
-                          keyboardType: TextInputType.visiblePassword,
-                          style: TextStyle(fontSize: 20),
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.lock,
-                                color: Colors.black,
-                              ),
-                              contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                              hintText: "Senha",
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: InputBorder.none,
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                        )),
-                  ),
-                  Padding(padding: EdgeInsets.all(3)),
-                  Material(
-                    elevation: 7,
-                    borderRadius: const BorderRadius.all(const Radius.circular(10.0)),
-                    color: Colors.white,
-                    child: Container(
-                        margin: const EdgeInsets.only(top: 0),
-                        child: TextField(
-                          obscureText: true,
-                          keyboardType: TextInputType.visiblePassword,
-                          style: TextStyle(fontSize: 20),
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.verified,
-                                color: Colors.black,
-                              ),
-                              contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                              hintText: "Confirme sua senha",
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: InputBorder.none,
-                              border: OutlineInputBorder(
-                                  borderRadius: const BorderRadius.all(const Radius.circular(10.0),
-                              ),
-                                  borderSide: BorderSide(color: Colors.white24),
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.black,
                             ),
+                            contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                            hintText: "Primeiro nome",
+                            filled: true,
+                            fillColor: Colors.white,
+                            focusedBorder: InputBorder.none,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
                           ),
                         )),
+                      ),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Material(
+                        color: Colors.transparent,
+                        borderRadius:
+                            BorderRadius.all(const Radius.circular(10.0)),
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 0),
+                            child: TextField(
+                              obscureText: true,
+                              keyboardType: TextInputType.phone,
+                              style: TextStyle(fontSize: 20),
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.phone_android,
+                                    color: Colors.black,
+                                  ),
+                                  contentPadding:
+                                      EdgeInsets.fromLTRB(32, 16, 32, 16),
+                                  hintText: "Telefone",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  focusedBorder: InputBorder.none,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            )),
+                      ),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Material(
+                        color: Colors.transparent,
+                        borderRadius:
+                            BorderRadius.all(const Radius.circular(10.0)),
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 0),
+                            child: TextField(
+                              obscureText: true,
+                              keyboardType: TextInputType.emailAddress,
+                              style: TextStyle(fontSize: 20),
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.email,
+                                    color: Colors.black,
+                                  ),
+                                  contentPadding:
+                                      EdgeInsets.fromLTRB(32, 16, 32, 16),
+                                  hintText: "E-mail",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  focusedBorder: InputBorder.none,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            )),
+                      ),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Material(
+                        color: Colors.transparent,
+                        borderRadius:
+                            BorderRadius.all(const Radius.circular(10.0)),
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 0),
+                            child: TextField(
+                              obscureText: true,
+                              keyboardType: TextInputType.visiblePassword,
+                              style: TextStyle(fontSize: 20),
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.black,
+                                  ),
+                                  contentPadding:
+                                      EdgeInsets.fromLTRB(32, 16, 32, 16),
+                                  hintText: "Senha",
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  focusedBorder: InputBorder.none,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            )),
+                      ),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Material(
+                        borderRadius:
+                            const BorderRadius.all(const Radius.circular(10.0)),
+                        color: Colors.white,
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 0),
+                            child: TextField(
+                              obscureText: true,
+                              keyboardType: TextInputType.visiblePassword,
+                              style: TextStyle(fontSize: 20),
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.verified,
+                                  color: Colors.black,
+                                ),
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(32, 16, 32, 16),
+                                hintText: "Confirme sua senha",
+                                filled: true,
+                                fillColor: Colors.white,
+                                focusedBorder: InputBorder.none,
+                                border: OutlineInputBorder(
+                                  borderRadius: const BorderRadius.all(
+                                    const Radius.circular(10.0),
+                                  ),
+                                  borderSide: BorderSide(color: Colors.white24),
+                                ),
+                              ),
+                            )),
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
+                      Container(
+                          margin: const EdgeInsets.only(top: 0),
+                          child: ElevatedButton(
+                            child: Text(
+                              "Cadastrar",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.green[400],
+                                elevation: 10,
+                                onPrimary: Colors.white,
+                                padding: EdgeInsets.all(18),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/login');
+                            },
+                          )),
+                    ],
                   ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  Container(
-                      margin: const EdgeInsets.only(top: 0),
-                      child: ElevatedButton(
-                        child: Text("Cadastrar", style: TextStyle(fontSize: 18),),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.orangeAccent,
-                            elevation: 10,
-                            onPrimary: Colors.white,
-                            padding: EdgeInsets.all(18),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                      )),
-                ],
+                ),
               ),
             ),
           ),
