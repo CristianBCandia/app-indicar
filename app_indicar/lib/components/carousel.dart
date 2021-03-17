4943f39ecea5392cf59fc9f4d89b5a0e6a9515d6
cbittenc <cristian.candia7@gmail.com>
2021-03-16 23:50:49 -0300

    Carousel Updated

--- app_indicar/lib/components/carousel.dart
+++ app_indicar/lib/components/carousel.dart
@@ -1,12 +1,24 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// items: [
-// 'images/client.jpeg',
-// 'images/buy.jpg',
-// 'images/money.jpg'
-// ]
-class CarouselHome extends StatelessWidget {
+
+class CarouselHome extends StatefulWidget {
+
+  @override
+  _CarouselHomeState createState() => _CarouselHomeState();
+}
+
+class _CarouselHomeState extends State<CarouselHome> {
+
+  int _current = 0;
+
+  List<String> text = [
+    'Indique um cliente...',
+    'E quando ele fizer uma compra pelo seu link...',
+    'Você receberá uma comissão!'
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +47,37 @@ class CarouselHome extends StatelessWidget {
                   Image.asset('images/money.jpg',
                     fit: BoxFit.cover,
                   ),
-                ]
+                ],
+                onImageChange: (prev, next) {
+                  setState(() {
+                     _current = next;
+                  });
+                },
               ),
-            )
+            ),
+            Card(
+              margin: new EdgeInsets.fromLTRB(10, 20, 10, 0),
+              elevation: 10,
+              semanticContainer: true,
+              child: Container(
+
+                padding: EdgeInsets.all(20.0),
+                color: Colors.deepPurpleAccent,
+                height: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(text[_current],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       );
